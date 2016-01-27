99e739848716b210d384ff2efbc928aa952101ab
jessica <jessica@exobi.com>
2016-01-26 23:28:33 -0500

    Move okvideo to vendor scripts, add js cookie

--- vvv/www/root-tongue/content/themes/root-tongue/library/enqueue-scripts.php
+++ vvv/www/root-tongue/content/themes/root-tongue/library/enqueue-scripts.php
@@ -5,35 +5,45 @@
  * Learn more about enqueue_script: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_script}
  * Learn more about enqueue_style: {@link https://codex.wordpress.org/Function_Reference/wp_enqueue_style }
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage FoundationPress
- * @since FoundationPress 1.0.0
+ * @since      FoundationPress 1.0.0
  */
 
 if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	function foundationpress_scripts() {
 
-	// Enqueue the main Stylesheet.
-	wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css' );
+		// Enqueue the main Stylesheet.
+		wp_enqueue_style( 'main-stylesheet', get_stylesheet_directory_uri() . '/assets/stylesheets/foundation.css' );
 
-	// Deregister the jquery version bundled with WordPress.
-	wp_deregister_script( 'jquery' );
+		// Deregister the jquery version bundled with WordPress.
+		wp_deregister_script( 'jquery' );
+		wp_deregister_style( 'login-with-ajax' );
 
-	// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
-	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
+		// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
+		wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js', array(), '2.1.0', false );
 
-	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 124-142.
-	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '2.0.0', true );
+		wp_enqueue_script( 'js-cookie', get_template_directory_uri() . '/assets/javascript/vendor/js.cookie.js', array( 'foundation' ), array(), true );
+		wp_enqueue_script( 'ok-video', get_template_directory_uri() . '/assets/javascript/vendor/okvideo.js', array(
+			'jquery',
+			'foundation'
+		), '2.3.2', true );
 
-	// Add the comment-reply library on pages where it is necessary
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+		// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 124-142.
+		// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
+		wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array(
+			'jquery',
+		), '2.0.0', true );
+
+		// Add the comment-reply library on pages where it is necessary
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+			wp_enqueue_script( 'comment-reply' );
+		}
 
 	}
 
 	add_action( 'wp_enqueue_scripts', 'foundationpress_scripts' );
+
 endif;
 
 ?>
